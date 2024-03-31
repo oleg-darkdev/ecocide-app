2b58ec8a56756e479becd83b7d659327dbb79b56
DarkDev <darkdev_brand@riseup.net>
2024-03-31 23:09:18 +0300

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import '../app.css';
+	import 'aos/dist/aos.css';
+	import AOS from 'aos';
+	import { onMount } from 'svelte';
+	onMount(() => {
+		AOS.init();
+	});
+</script>
+
+<main class="">
+	<slot />
+</main>
+
+<style>
+:global(.extra-bdr) {
+  border-radius: 5rem;
+}
+</style>
