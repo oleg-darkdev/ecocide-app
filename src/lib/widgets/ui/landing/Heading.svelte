1320a325f565f85a9d57ba59fc12307dced38fca
DarkDev <darkdev_brand@riseup.net>
2024-03-31 23:09:54 +0300

    alfa v.

---
+++ src/lib/widgets/ui/landing/Heading.svelte
@@ -0,0 +1,34 @@
+<script>
+  export let bgImage;
+</script>
+
+<section class="flex h-screen flex-col">
+	<div style='background-image: url({bgImage});' class="container-text px-10 font-black ">
+		Eco <br /> Bunt
+	</div>
+</section>
+
+<style>
+	@keyframes filling {
+		from {
+			background-position: center 25%;
+		}
+		to {
+			background-position: center 50%;
+		}
+	}
+
+	.container-text {
+		z-index: 0;
+
+		-webkit-text-fill-color: transparent;
+		-webkit-background-clip: text;
+		color: #ffffff;
+		padding-top: 20px;
+		font-size: 280px;
+		line-height: 0.7;
+		text-align: end;
+		/* font-family: 'Bungee', cursive; */
+		animation: filling 3s ease forwards;
+	}
+</style>
