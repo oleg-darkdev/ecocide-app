e0bf8943e2784d6cd0689cef239fe3bf33728068
DarkDev <darkdev_brand@riseup.net>
2024-03-31 23:10:16 +0300

    upd api

---
+++ src/lib/widgets/index.js
@@ -0,0 +1,9 @@
+import Hero from './ui/landing/Hero.svelte';
+import LettersSection from './ui/landing/LettersSection.svelte';
+import NightSection from './ui/landing/NightSection.svelte';
+import SkySection from './ui/landing/SkySection.svelte';
+import ForestSection from './ui/landing/ForestSection.svelte';
+import ContentSection from './ui/landing/ContentSection.svelte';
+import Heading from './ui/landing/Heading.svelte';
+
+export { Hero, LettersSection, NightSection, SkySection, ForestSection, ContentSection, Heading };
