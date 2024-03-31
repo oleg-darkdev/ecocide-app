b8c6a2f38301a454824c7ed08a5d04db877d61e6
DarkDev <darkdev_brand@riseup.net>
2024-03-31 23:10:30 +0300

    basic structure

---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+	import {
+		Hero,
+		Heading,
+		ForestSection,
+		LettersSection,
+		SkySection,
+		NightSection,
+		ContentSection
+	} from '$lib/widgets';
+</script>
+
+<!-- https://codepen.io/alvarotrigo/pen/KKvGNOj -->
+<!-- https://codepen.io/rukouen/details/wpgMZO -->
+
+<LettersSection />
+
+<Hero />
+
+<ContentSection>
+	<Heading bgImage={'https://static.pexels.com/photos/414171/pexels-photo-414171.jpeg'} />
+</ContentSection>
+
+<SkySection />
+
+<ContentSection>
+	<Heading bgImage={'https://static.pexels.com/photos/414171/pexels-photo-414171.jpeg'} />
+</ContentSection>
+
+<NightSection />
+
+<ContentSection>
+	<Heading bgImage={'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png'} />
+</ContentSection>
+
+<ForestSection />
