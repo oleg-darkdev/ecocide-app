184fa46789c3a8eb524bbfc8b06f2ab16d4fdb10
DarkDev <darkdev_brand@riseup.net>
2024-04-03 20:59:38 +0300

    add import of data & new comonents

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,7 @@
 <script>
+	import { ScenarioСard, OrganisationCard } from '$lib/entities';
+	import { scenarius, organisations } from '$lib/shared';
+
 	import {
 		Hero,
 		Heading,
@@ -6,31 +9,89 @@
 		LettersSection,
 		SkySection,
 		NightSection,
-		ContentSection
+		ContentSection,
+		Slider,
+		HorrorLetter
 	} from '$lib/widgets';
 </script>
 
 <!-- https://codepen.io/alvarotrigo/pen/KKvGNOj -->
 <!-- https://codepen.io/rukouen/details/wpgMZO -->
 
+<svelte:head>
+	<title>Ecocide boardgame</title>
+</svelte:head>
+
 <LettersSection />
 
 <Hero />
 
+<!-- <Slider /> -->
+
 <ContentSection>
-	<Heading bgImage={'https://static.pexels.com/photos/414171/pexels-photo-414171.jpeg'} />
+	<Heading
+		title="Night activism"
+		bgImage={'https://static.pexels.com/photos/414171/pexels-photo-414171.jpeg'}
+	/>
+
+	<section>
+		<div class=" mx-auto flex max-w-7xl flex-col items-center px-1 py-8 sm:px-6 ">
+			<div class="prose-blue prose mx-auto flex w-full max-w-3xl flex-col text-left">
+				<div class="mx-auto w-full">
+					<h1>A small headline to switch your visitors into users.</h1>
+					<h2>A small headline to switch your visitors into users.</h2>
+					<p>
+						Right. Say that again. No, no, George, look, it's just an act, right? Okay, so 9:00
+						you're strolling through the parking lot, you see us struggling.
+					</p>
+				</div>
+			</div>
+
+			<!-- <div class="flex flex-row justify-between"> -->
+			{#each scenarius as scenario}
+				<ScenarioСard {scenario} />
+			{/each}
+			<!-- </div> -->
+		</div>
+	</section>
 </ContentSection>
 
 <SkySection />
 
 <ContentSection>
-	<Heading bgImage={'https://static.pexels.com/photos/414171/pexels-photo-414171.jpeg'} />
+	<Heading
+		title="Night activism"
+		bgImage={'https://static.pexels.com/photos/414171/pexels-photo-414171.jpeg'}
+	/>
+	<section>
+		<div class=" mx-auto flex max-w-7xl flex-col items-center px-1 py-8 sm:px-6 ">
+			<div class="prose-blue prose mx-auto flex w-full max-w-3xl flex-col text-left">
+				<div class="mx-auto w-full">
+					<h1>A small headline to switch your visitors into users.</h1>
+					<h2>A small headline to switch your visitors into users.</h2>
+					<p>
+						Right. Say that again. No, no, George, look, it's just an act, right? Okay, so 9:00
+						you're strolling through the parking lot, you see us struggling.
+					</p>
+				</div>
+			</div>
+			<div class="flex flex-row ">
+				{#each organisations as organisation}
+					<OrganisationCard {organisation} />
+				{/each}
+			</div>
+		</div>
+	</section>
 </ContentSection>
 
 <NightSection />
 
 <ContentSection>
-	<Heading bgImage={'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png'} />
+	<Heading
+		title="Night activism"
+		bgImage={'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png'}
+	/>
+	<HorrorLetter />
 </ContentSection>
 
 <ForestSection />
