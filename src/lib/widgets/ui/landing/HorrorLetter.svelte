6a44b237fae2394b73da65f974eed0742e19a372
DarkDev <darkdev_brand@riseup.net>
2024-04-03 20:56:36 +0300

    change ttitle text

---
+++ src/lib/widgets/ui/landing/HorrorLetter.svelte
@@ -0,0 +1,15 @@
+<div class="w-full flex justify-end items-end">
+  <h1 class="main-title text-black font-bold text-green-700">
+	"Here, in the forest,<br /><span style="padding-left:100px">dark and deep,</span><br /><span
+		style="padding-right:110px">I offer you,</span
+	><br /><span style="padding-left:-20px">eternal sleep."</span>
+</h1>
+</div>
+
+<style>
+	.main-title {
+		padding: 0.3em 1em 0.25em;
+		font-size: 40px;
+		line-height: 1.3;
+	}
+</style>
