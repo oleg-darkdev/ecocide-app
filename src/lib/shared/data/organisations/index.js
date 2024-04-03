6b2e33a9e15c38daa4521a1d810996db82ef81f2
DarkDev <darkdev_brand@riseup.net>
2024-04-03 20:55:50 +0300

    add new organisations

---
+++ src/lib/shared/data/organisations/index.js
@@ -0,0 +1,21 @@
+import seaShepherd from './seaShepherd';
+
+const arrayOrgs = [seaShepherd, seaShepherd];
+
+export default arrayOrgs;
+
+
+// Country of origin
+// Years of activity
+//  Country affiliates
+// Philosophy
+// Scope of activity (лес, море, города)
+// Year of foundation
+// logo
+// википедия ру, ен
+// shortDesc
+// desc
+// officialSite
+// socialMedia
+// amountActions
+// activities []
---
+++ src/lib/shared/data/organisations/seaShepherd.js
@@ -0,0 +1,87 @@
+const data = {
+	officialSite: 'https://seashepherd.org/',
+	yearOfFoundation: 1977,
+	logo: 'sea_shepherd.jpg',
+	tags: ['marine', 'sea', 'seal hunting', 'fishing'],
+
+	goals: ['Marine conservation activism.'],
+	title: 'Sea Shepherd Conservation Society or Sea Shepherd',
+	abbr: 'SCSS',
+	placeOfOrigin: 'United State',
+	yearsofActivity: 'present',
+	countryAffiliates: ['United Kingdom', 'United State', 'Australia', '', '', '', ''],
+	areaOfActivity: [
+		'Global',
+		'South Pacific',
+		'Mediterranean',
+		'waters around the Galapagos Island'
+	],
+	principles: [{ title: '', desc: '' }],
+
+	base: {
+		title: 'United State, Washington, Friday Harbor on San Juan Island',
+		link: ''
+	},
+	scopeOfActivity: [
+		{
+			title: '',
+			img: '',
+			desc: ''
+		}
+	],
+	history: ['', '', '', ''],
+	organization: ['', '', '', ''],
+
+	actions: [
+		{
+			title: '',
+			date: '',
+			img: '',
+			desc: ['', '', '']
+		}
+	],
+	wikipedia: [
+		{
+			language: 'Russian',
+			link: 'https://ru.wikipedia.org/wiki/Sea_Shepherd'
+		},
+		{
+			language: 'English',
+			link: 'https://en.wikipedia.org/wiki/Sea_Shepherd_Conservation_Society'
+		}
+	],
+	shortDesc:
+		'Sea Shepherd engages in conventional protests and direct actions to protect marine wildlife. ',
+	desc: [
+		`Sea Shepherd operations have included interdiction against commercial fishing, shark poaching and finning, seal hunting, and whaling. `,
+		`In addition to their direct action campaigns, Sea Shepherd works on ocean issues such as plastic pollution. Sea Shepherd chapters across the world organize onshore cleanups throughout the year to pick up debris near oceans, streams, and rivers.`,
+		`In addition to the organization's role of documenting and reporting violations of conservation laws, Sea Shepherd operations have utilized direct, non-lethal tactics including scuttling and disabling whaling vessels at harbor, intervening in Canadian and Namibian seal hunts, shining laser light at whalers, throwing bottles of foul-smelling butyric acid onto vessels at sea, boarding whaling vessels to protest while at sea, and seizing and destroying drift nets at sea.`,
+		`Sea Shepherd argues that these tactics are necessary to uncover and impede actions that violate international laws protecting wildlife, as the international community has shown itself unwilling or unable to stop species-endangering whaling and fishing practices.`,
+		`In 2009, Paul Watson claimed the organization has sunk ten whaling ships while also destroying millions of dollars worth of equipment.`
+	],
+	socialMedia: [
+		{
+			link: '',
+			title: ''
+		}
+	],
+	typesOfActivities: [
+		{
+			title: 'Direct action',
+			img: '',
+			desc: ''
+		},
+		{
+			title: 'Active obstruction of fishing vessels',
+			img: '',
+			desc: ''
+		},
+		{
+			title: 'Tracking of irregularities and reporting',
+			img: '',
+			desc: ''
+		}
+	]
+};
+
+export default data;
