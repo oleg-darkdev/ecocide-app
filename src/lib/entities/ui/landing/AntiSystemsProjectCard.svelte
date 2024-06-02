791d852440df560d09c138d4349af4549ee20356
DarkDev <darkdev_brand@riseup.net>
2024-06-03 01:11:34 +0300

    upd content

--- src/lib/entities/ui/landing/AntiSystemsProjectCard.svelte
+++ src/lib/entities/ui/landing/AntiSystemsProjectCard.svelte
@@ -1,7 +1,7 @@
 <div class="card max-w-xl2 bg-base-100 shadow-xl">
   <div class="card-body">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <!-- <h2 class="card-title">Antisystem Boardgames</h2> -->
+    <p>Ten produkt jest częścią projektu <a href="" class='font-bold text-xl text-teal-800' target="_blank" >Antisystem Boardgames</a>, aplikacja internetowa opracowana przez <a href="https://darkdev-foundation.vercel.app/" class='font-bold text-xl text-teal-800' target="_blank" >DarkDev Foundation</a>, produkcja gry planszowej przez <a href="https://44games.vercel.app/" target="_blank" class='font-bold text-xl text-teal-800'>44Games</a>. </p>
   </div>
-  <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <figure><img src="" alt="Antisystem Boardgames" /></figure>
 </div>
