aab1af60b1cb951edd8ea2aa8727d6e0d800eeac
DarkDev <darkdev_brand@riseup.net>
2024-06-03 01:10:58 +0300

    upd api

--- src/lib/shared/data/organisations/index.js
+++ src/lib/shared/data/organisations/index.js
@@ -1,6 +1,24 @@
 import seaShepherd from './seaShepherd';
+import extinctionRebellion from './extinctionRebellion';
+import earthFirst from './earthFirst';
+import animalLiberationFront from './animalLiberationFront';
+import earthLiberationFront from './earthLiberationFront';
+import rainbowKeepers from './rainbowKeepers';
+import theMushroomElves from './theMushroomElves';
+import lastGeneration from './lastGeneration';
+// import lastGeneration from './lastGeneration';
+// import lastGeneration from './lastGeneration';
 
-const arrayOrgs = [seaShepherd, seaShepherd];
+const arrayOrgs = [
+	seaShepherd,
+	extinctionRebellion,
+	earthFirst,
+	rainbowKeepers,
+	animalLiberationFront,
+	earthLiberationFront,
+  theMushroomElves,
+  lastGeneration
+];
 
 export default arrayOrgs;
 
