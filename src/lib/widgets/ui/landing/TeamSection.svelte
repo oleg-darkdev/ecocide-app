7b53de85a30265fae4696dfb7bec71e271a2f823
DarkDev <darkdev_brand@riseup.net>
2024-06-03 01:07:35 +0300

    add team member card

--- src/lib/widgets/ui/landing/TeamSection.svelte
+++ src/lib/widgets/ui/landing/TeamSection.svelte
@@ -4,11 +4,39 @@
 	import { teamMembers } from '$lib/shared';
 </script>
 
-<section class="container mx-auto px-6 pt-10">
-	<p class="mx-auto my-6 max-w-2xl text-center text-gray-500 ">
-		Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo incidunt ex placeat modi magni
-		quia error alias, adipisci rem similique, at omnis eligendi optio eos harum.
-	</p>
+<!--
+  Наша команда — это больше, чем просто разработчики игры. Это сообщество людей, объединенных одной целью — сделать мир лучше.
+
+В команду проекта входят экологи, социальные активисты, эко-анархисты и представители различных экологических организаций. Это люди, которые ежедневно вносят свой вклад в чистоту наших рек, лесов и воздуха, за права животных и сохранение биоразнообразия.
+
+Мы верим, что только вместе, объединив усилия, мы сможем добиться реальных изменений. Игра, которую мы создали — это наш вклад в это великое дело. Мы приглашаем вас присоединиться к нам и стать частью экологического движения, играя и внося свой вклад вместе с нами.
+ -->
+<section class="container mx-auto lg:px-6 pt-10">
+  <div class="prose  prose-blue mx-auto flex w-full max-w-2xl flex-col text-left">
+			<div class="px-2  w-full">
+				<!-- <h1>Nasz świat się zmienia i każdy z nas odczuwa te zmiany.</h1>
+				<h2>
+					To nie tylko edukacyjna gra planszowa wzbogacona o aplikację internetową - to szansa na
+					zrozumienie.
+				</h2> -->
+				<p>
+							Nasz zespół to coś więcej niż tylko niezależni specjaliści. To społeczność ludzi zjednoczonych jednym celem - zmienić świat na lepsze.
+
+				</p>
+				<p>
+          W skład zespołu pracującego nad grą wchodzą ekolodzy, aktywiści społeczni, eko-anarchiści i przedstawiciele różnych organizacji ekologicznych. Są to ludzie, którzy każdego dnia przyczyniają się do czystości naszych rzek, lasów i powietrza, praw zwierząt i ochrony bioróżnorodności.
+				</p>
+        	<p>
+            Wierzymy, że tylko razem, łącząc siły, możemy dokonać prawdziwej zmiany. Stworzona przez nas gra jest naszym wkładem w tę wielką sprawę. Zapraszamy do dołączenia do nas i stania się częścią ruchu na rzecz ochrony środowiska, grając z nami i wnosząc swój wkład.
+				</p>
+
+
+
+			</div>
+
+		</div>
+	<!-- <p class="mx-auto my-6 max-w-2xl text-center text-gray-500 ">
+	</p> -->
 
 	<a name="team" />
 
