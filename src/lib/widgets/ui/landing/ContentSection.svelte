6f7cb5b65f4ae61b346bec9b4588487990f5025e
DarkDev <darkdev_brand@riseup.net>
2024-06-03 01:10:29 +0300

    upd paddings

--- src/lib/widgets/ui/landing/ContentSection.svelte
+++ src/lib/widgets/ui/landing/ContentSection.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <section
-	class="extra-bdr relative mx-auto min-h-screen -mt-[20vh] z-[99]  max-w-4xl bg-white"
+	class="extra-bdr relative py-10 mx-auto min-h-screen -mt-[20vh] z-[99]  max-w-4xl bg-white"
 	
 >
 	<slot />
