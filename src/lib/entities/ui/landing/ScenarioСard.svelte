53cf680d6b699d695a0fa24e2548590f703590bf
DarkDev <darkdev_brand@riseup.net>
2024-06-11 20:09:45 +0300

    fix text-color, paddings etc

--- src/lib/entities/ui/landing/ScenarioСard.svelte
+++ src/lib/entities/ui/landing/ScenarioСard.svelte
@@ -6,13 +6,13 @@
 </script>
 
 <div
-	class="group mx-auto my-10   flex max-w-7xl flex-col items-center rounded-md border-b border-gray-200 p-2 pb-10 hover:bg-teal-800 hover:-translate-y-1 hover:scale-110 hover:transition hover:duration-1000 hover:ease-in-out sm:flex-row"
+	class="group mx-auto py-5  extra-bdr flex max-w-7xl flex-col items-center  border-b border-gray-200 p-2 pb-10 group hover:bg-teal-800 hover:-translate-y-1 hover:scale-110 hover:transition hover:duration-1000 hover:ease-in-out sm:flex-row"
 >
 	<div
 		class="inline-flex h-20 w-20 flex-shrink-0 items-center justify-center rounded-full bg-gray-50 text-neutral-600 sm:mr-10"
 	>
 		<div class="avatar">
-			<div class="w-24 rounded-full">
+			<div class="w-24 p-2 rounded-full">
 				<img src="/images/icons/{scenario.icon}" alt="icon type" />
 			</div>
 		</div>
@@ -20,11 +20,11 @@
 	<div class="prose-sm prose mt-0 flex-grow text-center sm:mt-0 sm:text-left">
 		<div class="-mb-8">
 			{#each scenario.tags as tag}
-				<div class="badge-outline badge mr-2">{tag}</div>
+				<div class="badge-outline badge mr-2 text-[#115e59] group-hover:text-neutral-200">{tag}</div>
 			{/each}
 		</div>
-		<h2>{scenario.title}</h2>
-		<p>
+		<h2 class='text-[#115e59] group-hover:text-neutral-200'>{scenario.title}</h2>
+		<p class='text-[#115e59] group-hover:text-neutral-200'>
 			{scenario.shortDesc}
 		</p>
 	</div>
