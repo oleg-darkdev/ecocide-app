55a74cdf047c036c204c836a3abcec79dc47fc2b
DarkDev <darkdev_brand@riseup.net>
2024-06-11 20:09:06 +0300

    add content, upd link

--- src/lib/entities/ui/landing/AntiSystemsProjectCard.svelte
+++ src/lib/entities/ui/landing/AntiSystemsProjectCard.svelte
@@ -1,7 +1,7 @@
-<div class="card max-w-xl2 bg-base-100 shadow-xl">
-  <div class="card-body">
+<div class="card max-w-xl  bg-base-100 shadow-xl">
+  <div class="card-body text-center text-neutral-600">
     <!-- <h2 class="card-title">Antisystem Boardgames</h2> -->
-    <p>Ten produkt jest częścią projektu <a href="" class='font-bold text-xl text-teal-800' target="_blank" >Antisystem Boardgames</a>, aplikacja internetowa opracowana przez <a href="https://darkdev-foundation.vercel.app/" class='font-bold text-xl text-teal-800' target="_blank" >DarkDev Foundation</a>, produkcja gry planszowej przez <a href="https://44games.vercel.app/" target="_blank" class='font-bold text-xl text-teal-800'>44Games</a>. </p>
+    <p>Ten produkt jest częścią projektu <a href="https://anti-system.vercel.app/" class='font-bold  text-teal-800' target="_blank" >Antisystem Boardgames</a>, aplikacja internetowa opracowana przez <a href="https://darkdev-foundation.vercel.app/" class='font-bold  text-teal-800' target="_blank" >DarkDev Foundation</a>, produkcja gry planszowej przez <a href="https://44games.vercel.app/" target="_blank" class='font-bold text-teal-800'>44Games</a>. </p>
   </div>
-  <figure><img src="" alt="Antisystem Boardgames" /></figure>
+  <figure><img src="/images/anti-system.jpg" alt="Antisystem Boardgames" /></figure>
 </div>
